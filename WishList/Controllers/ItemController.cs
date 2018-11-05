664d37ba023ac9c868079d8df30a280d0c5f884a
Randy Kline <Randy.Kline@ABILITYNetwork.com>
2018-11-05 16:57:09 -0500

    5

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var itemToRemove = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Remove(itemToRemove);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
